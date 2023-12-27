97c045417e2e5446bf873d76451c652315423934
Nayan-alpha <149183073+Nayan-alpha@users.noreply.github.com>
2023-12-27 11:51:28 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -81,28 +81,30 @@ function Home() {
 
   const clearHandler = () => {
     setImgFile(null);
-    // setXrayImage(defaultImage);
+    setXrayImage(defaultImage);
     setResponseMessage("Generated Text");
     setTarget("");
   };
 
   return (
     <div id="img-container4">
-      <img src={"https://plus.unsplash.com/premium_photo-1673306778968-5aab577a7365?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="background" />
+      <img src={bg} alt="background" />
       <div id="img-overlay4"></div>
       <div id="dashboard-container">
         <div id="pred">
           <div id="xray">
             <img
-              src={"https://plus.unsplash.com/premium_photo-1673306778968-5aab577a7365?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+              src={xrayImage}
               alt="X-ray Image"
               style={{ width: "100%", height: "100%" }}
             />
           </div>
           <div id="output">
-            
+            {loading ? (
+              <img src={loadingIcon} alt="Loading" id="loader" />
+            ) : (
               <div className="output-text">{responseMessage}</div>
-            
+            )}
           </div>
         </div>
 
