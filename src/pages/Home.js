5e2edc19adacd6f35d59420794447ab9ffe14401
Nayan-alpha <149183073+Nayan-alpha@users.noreply.github.com>
2023-12-27 10:11:48 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,155 +1,82 @@
-import React, { useState } from 'react'
-// import { toast } from 'react-hot-toast'
-import './Home.css'
-import axios from 'axios'
-
+import React, { useState } from 'react';
+import axios from 'axios';
+import {./Home.css}
 
 function Home() {
-    // const [imgFile, setImgFile] = useState(null);
-    // const [responseMessage,setResponseMessage] = useState("-----")
-    // const[imageURL,setImageURL] = useState(null);
-    // const fileSelectHandler = (event) => {
-    //     setImgFile(event.target.files[0]);
-    //     setImageURL(URL.createObjectURL(event.target.files[0]));
-    // };
-    // const fileUploadHandler = async () => {
-    //     if (imgFile) {
-    //     const formData = new FormData();
-    //     formData.append("image", imgFile);
+  const [image, setImage] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [target, setTarget] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
 
-    //     try {
-    //         const response = await fetch("https://barely-ruling-whale.ngrok-free.app/upload", {
-    //         method: "POST",
-    //         body: formData,
-    //         });
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageURL(URL.createObjectURL(file));
+    setImage(file);
+  };
 
-    //         if (response.ok) {
-    //         console.log("Image uploaded successfully!");
-    //         const data = await response.text();
-    //         setResponseMessage(data)
-    //         } else {
-    //         console.error("Image upload failed.");
-    //         }
-    //     } catch (error) {
-    //         console.error("Error during image upload:", error);
-    //     }
-    //     }
-    //     else {
-    //     console.log('No file selected.');
-    // }
-    // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    // return (
-    //     <div class="container">
-    //     <h1>Upload Image</h1>
-    //     <input type="file" onChange={fileSelectHandler}></input>
-    //     <button onClick={fileUploadHandler}>Generate</button>
-    //     {imageURL && (
-    //     <div>
-    //       <h2>Selected Image Preview</h2>
-    //       <img src={imageURL} alt="Selected" style={{ maxWidth: '100%', maxHeight: '300px' }} />
-    //     </div>
-    //     )}
-    //     <h2>Output: {responseMessage}</h2>
-        
-    //     </div>
-    // );
-    const [image, setImage] = useState(null);
-    const [prediction, setPrediction] = useState(null);
-    const [target, setTarget] = useState(null);
-    const[imageURL,setImageURL] = useState(null);
+    try {
+      if (image) {
+        const formData = new FormData();
+        formData.append('image', image);
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        setImageURL(URL.createObjectURL(e.target.files[0]));
-        setImage(file);
-    };
+        const response = await axios.post(
+          'https://barely-ruling-whale.ngrok-free.app/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        );
 
-    const handlesubmit = async (e) => {
-        e.preventDefault();
-         try {
-             const response = await fetch("https://barely-ruling-whale.ngrok-free.app/upload", {
-             method: "POST",
-             body: formData,
-            });
-
-            if (response.ok) {
-             console.log("Image uploaded successfully!");
-             const data = await response.text();
-             setResponseMessage(data)
-             } else {
-             console.error("Image upload failed.");
-             }
-         } catch (error) {
-             console.error("Error during image upload:", error);
-         }
-         }
-         else {
-         console.log('No file selected.');
-     }
-     };
-        // try {
-        //     const reader = new FileReader();
-        //     reader.onloadend = () => {
-        //         const base64data = reader.result.split(',')[1];
-
-        //         const requestData = {
-        //             image: base64data,
-        //             name: image.name || 'default_filename.jpg',
-        //         };
-
-
-        //         axios.post("https://barely-ruling-whale.ngrok-free.app/upload",requestData, {
-        //             withCredentials: true,
-        //         })
-        //             .then((response) => {
-        //                 const result = response;
-                        
-        //                 console.log(Object.keys(result))
-                        
-        //                 setPrediction(result["data"]["prediction"])
-        //                 setTarget(result["data"]["target"])
-        //                 console.log('Response from server:', result);
-        //             })
-        //             .catch((error) => {
-        //                 console.error('Error:', error);
-        //             });
-        //     };
-
-        //     reader.readAsDataURL(image);
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        if (response.status === 200) {
+          console.log('Image uploaded successfully!');
+          const result = response.data;
+          setPrediction(result.prediction);
+          setTarget(result.target);
+        } else {
+          console.error('Image upload failed.');
+        }
+      } else {
+        console.log('No file selected.');
+      }
+    } catch (error) {
+      console.error('Error during image upload:', error);
     }
-    return (
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='image'>Upload image</label>
+        <input type='file' id='image' onChange={handleImageChange} />
+        <button type='submit'>Submit</button>
+      </form>
+
+      {imageURL && (
         <div>
-            <form onSubmit={handlesubmit}>
-                <label htmlFor='image'>Upload image</label>
-                <input type='file' id="image" onChange={handleImageChange}></input>
-                <button type="submit">Submit</button>
-            </form>
-
-            {imageURL && (
-                <div>
-                    <h2>Selected Image Preview</h2>
-                    <img src={imageURL} alt="Selected" style={{ maxWidth: '100%', maxHeight: '300px' }} />
-                </div>
-             )}
-            {prediction ? (
-                <div className='output'>
-                    
-                    <h2>prediction</h2>
-                    {prediction}
-                    <h2>target</h2>
-                    {target}
-                </div>
-            ) : null}
-
+          <h2>Selected Image Preview</h2>
+          <img
+            src={imageURL}
+            alt='Selected'
+            style={{ maxWidth: '100%', maxHeight: '300px' }}
+          />
         </div>
+      )}
 
-
-
-    )
+      {prediction ? (
+        <div className='output'>
+          <h2>Prediction</h2>
+          {prediction}
+          <h2>Target</h2>
+          {target}
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
